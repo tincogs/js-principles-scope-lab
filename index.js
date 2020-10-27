46fdd5fa1cf5351881fc3a0bfb87b58cfa00b51b
tincogs <tomoday91@gmail.com>
2020-10-27 13:14:03 -0500

    commit tree 3bedbddfc7b8c402982c5584d5908cf895243512

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "sherm"
+var bestCustomer;
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'billy'
+}
